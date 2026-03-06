e01ea5d62a96da689a21aeaa187d79f27529f37b
tagiyevtehmasib <tagiyevtehmasib1289@gmail.com>
2026-03-06 19:45:50 +0400

    Part-1 : Customer Segmentation

--- RFM Analysis.sql
+++ RFM Analysis.sql
@@ -26,7 +26,7 @@ ScoringDate AS
 	NTILE(5) OVER(ORDER BY max_order DESC) AS max_score
 	FROM RecencyMinus
 )
-SELECT * FROM ScoringDate
+SELECT * FROM ScoringDate 
 ORDER BY max_order ASC
 
 
@@ -43,12 +43,12 @@ GROUP BY CustomerID
 ),
 FrequencyScoring AS 
 (
-	SELECT unique_customer AS DG,
-	total_order AS F,
+	SELECT unique_customer AS unique_customer,
+	total_order AS totalorder,
 	NTILE(5) OVER(ORDER BY total_order ASC) AS score_total_order
 	FROM Frequency
 )
-SELECT * FROM FrequencyScoring
+SELECT * FROM FrequencyScoring 
 
 
 -------------------------------------------------------------------------------------------
@@ -69,7 +69,7 @@ MonetaryScoring AS
 	NTILE(5) OVER(ORDER BY m_totaldue ASC) AS m_ntilescoring
 	FROM Monetary
 )
-SELECT * FROM MonetaryScoring
+SELECT * FROM MonetaryScoring 
 
 
 -------------------------------------------------------------------------------------------
@@ -134,10 +134,114 @@ JOIN FScoring f
 ON f.funique_customer = m.m_customerID
 ORDER BY m.m_customerID
 
+-----------------------------------------------------------------------------
+
+CREATE VIEW RFM1 AS
+WITH RecencyOrder AS 
+(
+	SELECT CustomerID AS last_customer,
+	OrderDate AS last_order,
+	ROW_NUMBER() OVER(PARTITION BY CustomerID ORDER BY OrderDate DESC) AS rank_order
+	FROM Sales.SalesOrderHeader
+),
+RecencyMinus AS
+(
+	SELECT last_customer AS max_customer,
+	DATEDIFF(DAY, last_order, '2014-07-3') AS max_order
+	FROM RecencyOrder
+	WHERE rank_order = 1
+),
+RScoring AS
+(
+	SELECT max_customer,
+	max_order,
+	NTILE(5) OVER(ORDER BY max_order DESC) AS R
+	FROM RecencyMinus
+),
+Frequency AS
+(
+	SELECT CustomerID AS unique_customer,
+	COUNT(SalesOrderID) AS total_order
+	FROM Sales.SalesOrderHeader
+	GROUP BY CustomerID
+),
+FScoring AS 
+(
+	SELECT unique_customer AS funique_customer,
+	total_order AS ftotal_order,
+	NTILE(5) OVER(ORDER BY total_order ASC) AS F
+	FROM Frequency
+),
+Monetary AS 
+(
+	SELECT CustomerID AS m_customerID,
+	SUM(TotalDue) AS m_totaldue
+	FROM Sales.SalesOrderHeader
+	GROUP BY CustomerID
+),
+MScoring AS 
+(
+	SELECT m_customerID AS m_customerID,
+	m_totaldue AS m_totaldue,
+	NTILE(5) OVER(ORDER BY m_totaldue ASC) AS M
+	FROM Monetary
+)
+SELECT m.m_customerID AS m_customerID,
+r.R AS R1,
+f.F AS F1,
+m.M AS M1
+FROM MScoring m
+JOIN RScoring r
+ON r.max_customer = m.m_customerID
+JOIN FScoring f
+ON f.funique_customer = m.m_customerID
+
+------------------------------------------------------------------------------------------------------------
+
+--But in this section, I evaluated the result with 3 main parameters using the keywords "between and".
+
+
+SELECT m_customerID,
+(R1 + F1 + M1) as total_rfm,
+CONCAT_WS('', R1, F1, M1) AS RFM1,
+CASE 
+	WHEN (R1 + F1 + M1) BETWEEN 13 AND 15 THEN 'Qızıl' 
+	WHEN (R1 + F1 + M1) BETWEEN 8 AND 12 THEN 'Gümüş'
+	ELSE 'Bürünc'
+END AS Dereceler
+FROM RFM1
+
+------------------------------------------------------------------------------------------------------------
+
+--I had created in mistake form.
+--SELECT * FROM RFM
+--DROP VIEW RFM
+
+------------------------------------------------------------------------------------------------------------
+
+--I had to write many segment here, so I didn't write all of them.
+--SELECT m_customerID,
+--CONCAT_WS('', R1, F1, M1),
+--CASE
+--	WHEN R1 = 5 AND F1 = 5 AND M1 = 5 THEN 'Çempionlar'
+--	WHEN (R1 = 4 OR R1 = 5) AND (F1 = 4 OR F1 = 5) AND (M1 = 4 OR M1 = 5) THEN 'Sadiq Müştərilər'
+--	WHEN (R1 = 3 OR R1 = 4) AND (F1 = 3 OR F1 = 4) AND (M1 = 3 OR M1 = 4) THEN 'Potensial Sadiqlər'
+--	WHEN R1 = 5 AND (F1 = 1 OR F1 = 2) AND (M1 = 1 OR M1 = 2) THEN 'Yeni Müştərilə'
+--	WHEN (R1 = 2 OR R1 = 3) AND (F1 = 2 OR F1 = 3) AND (M1 = 2 OR M1 = 3) THEN 'Risk Altında Olan Müştərilər'
+--	WHEN (R1 = 1 OR R1 = 2) AND F1 = 1 AND M1 = 1 THEN 'İtirilmiş Müştərilər'
+--	ELSE NULL
+--END
+--FROM RFM1
+
+------------------------------------------------------------------------------------------------------------
 
 
 
 
+SELECT CustomerID,
+SUM(TotalDue)
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID
 
 
 
