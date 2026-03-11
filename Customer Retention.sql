1a90f3a3c118e0ff7299dde3159d6fd6bc739f55
tagiyevtehmasib <tagiyevtehmasib1289@gmail.com>
2026-03-11 16:59:28 +0400

    New determined customer

--- Customer Retention.sql
+++ Customer Retention.sql
@@ -1,5 +1,11 @@
 
 
-SELECT * FROM Sales.SalesOrderHeader
 
-SELECT * FROM Sales.Customer
+SELECT CustomerID,
+MIN(OrderDate),
+DATEFROMPARTS(YEAR(MIN(OrderDate)), MONTH(MIN(OrderDate)), 1) AS montly_cohort
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2011
+GROUP BY CustomerID
+ORDER BY MIN(OrderDate)
+
